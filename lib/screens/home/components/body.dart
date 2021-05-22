e6dde2b4af6b42aa04af1c06e702ce9a6dce46ca
TejasPatel3300 <77225883+TejasPatel3300@users.noreply.github.com>
2021-05-22 15:08:27 +0530

    section title finished

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,8 +1,10 @@
 import 'package:ecommerce/size_config.dart';
 import 'package:flutter/material.dart';
 
+import 'categories.dart';
 import 'discount_banner.dart';
 import 'home_header.dart';
+import 'section_title.dart';
 
 class Body extends StatelessWidget {
   @override
@@ -15,17 +17,24 @@ class Body extends StatelessWidget {
             HomeHeader(),
             SizedBox(height: getProportionateScreenWidth(30)),
             DiscountBanner(),
+            SizedBox(height: getProportionateScreenWidth(30)),
             Categories(),
+            SizedBox(height: getProportionateScreenWidth(30)),
+            SectionTitle(
+              text: "Special for you",
+              press: () {},
+            ),
+            SizedBox(
+              width: getProportionateScreenWidth(240),
+              height: getProportionateScreenWidth(100),
+              child: Image.asset(
+                "assets/images/Image Banner 2.png",
+                fit: BoxFit.cover,
+              ),
+            ),
           ],
         ),
       ),
     );
-  }
-}
-
-class Categories extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
